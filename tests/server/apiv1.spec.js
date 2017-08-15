446a52dbe1f1f8915ef6260a5bae66642723e34b
jparra5 <jparra@us.ibm.com>
2017-08-15 16:24:47 -0500

    Update apiv1.spec.js

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -108,7 +108,7 @@
       apiv1.getWeather(reqMock, resMock);
 
       assert(resMock.status.lastCall.calledWith(200), 'Unexpected response:' + resMock.status.lastCall.args);
-      assert(resMock.send.lastCall.args[0].city === 'El Paso', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
+      assert('Austin' === 'El Paso', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
       assert(resMock.send.lastCall.args[0].weather === 'Conditions are cold and temperature is 78 F', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
     });
   });
